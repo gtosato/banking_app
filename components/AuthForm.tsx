96b7d37efe5daa3e726a6b53a1a78f62c31e6c9d
Greg Tosato <gregtosato@gmail.com>
2024-06-02 23:55:37 +1000

    changes to sign-in auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,7 +21,7 @@ import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
 import SignUp from '@/app/(auth)/sign-up/page'
 import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.actions'
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
 
 
 const AuthForm = ({ type }: { type: string }) => {
@@ -52,12 +52,12 @@ const AuthForm = ({ type }: { type: string }) => {
             }
 
             if (type === 'sign-in') {
-                // const response = await signIn({
-                //     email: data.email,
-                //     password: data.password,
-                // })
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password,
+                })
                 
-                // if(response) router.push('/')
+                if(response) router.push('/')
             }
 
         } catch (error) {
