f5a499faafc75975bddbd8706fd188fe09106243
Greg Tosato <gregtosato@gmail.com>
2024-06-08 21:37:56 +1000

    update to AuthForm

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -46,6 +46,20 @@ const AuthForm = ({ type }: { type: string }) => {
 
         try {
 
+            // Sign up with Appwrite & create plaid token
+            const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                tfn: data.taxFileNumber!,
+                email: data.email,
+                password: data.password
+            }
+
             if (type === 'sign-up') {
                 const newUser = await signUp(data);
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -8,7 +8,7 @@ import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestPr
 import { plaidClient } from "@/lib/plaid"
 import { AwardIcon, Car } from "lucide-react"
 import { revalidatePath } from "next/cache"
-import { addFundingSource } from "./dwolla.actions"
+import { addFundingSource, createDwollaCustomer } from "./dwolla.actions"
 
 const {
     APPWRITE_DATABASE_ID: DATABASE_ID,
@@ -31,16 +31,41 @@ export const signIn = async ({ email, password } : signInProps) => {
 export const signUp = async (userData: SignUpParams) => {
     const { email, password, firstName, lastName } = userData;
 
+    let newUserAccount 
     try {
-        const { account } = await createAdminClient();
+        const { account, database } = await createAdminClient();
 
-        const newUserAccount = await account.create(
+        newUserAccount = await account.create(
             ID.unique(),
             email,
             password,
             `${firstName} ${lastName}`    
         );
 
+        if(!newUserAccount) throw new Error('Error creaing user')
+
+        const dwollaCustomerUrl = await createDwollaCustomer({
+            ...userData,
+            type: 'personal'
+        })
+
+        if (!dwollaCustomerUrl) throw new Error('Error creating Dwolla custoemr')
+        
+        const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+
+        const newUser = await database.createDocument(
+            DATABASE_ID!,
+            USER_COLLECTION_ID!,
+            ID.unique(),
+            {
+                ...userData,
+                userId: newUserAccount.$id,
+                dwollaCustomerId,
+                dwollaCustomerUrl,
+
+            }
+        )
+        
         const session = await account.createEmailPasswordSession(email, password);
 
         cookies().set("appwrite-session", session.secret, {
@@ -130,7 +155,7 @@ export const createBankAccount = async({
         )
 
         return parseStringify(bankAccount);
-        
+
     } catch (error) {
         
     }
