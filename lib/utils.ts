e070acc58969bcf228ea72850eba652bf571e198
Greg Tosato <gregtosato@gmail.com>
2024-06-13 23:51:48 +1000

    updated validations

--- lib/utils.ts
+++ lib/utils.ts
@@ -201,7 +201,7 @@ export const authFormSchema = (type: string) => z.object({
   lastName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   address1: type === 'sign-in' ? z.string().optional() : z.string().max(50),
   city: type === 'sign-in' ? z.string().optional() : z.string().max(20),
-  state: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(3),
+  state: type === 'sign-in' ? z.string().optional() : z.string().min(2).max(2),
   postalCode: type === 'sign-in' ? z.string().optional() : z.string().min(3).max(6),
   dateOfBirth: type === 'sign-in' ? z.string().optional() : z.string().min(3),
   taxFileNumber: type === 'sign-in' ? z.string().optional() : z.string().min(3),   
